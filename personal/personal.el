6b596f96a01b7aceb1e6cc1017b64a84562e02bf
Amir Eldor <amir@eize.ninja>
2018-07-06 03:22:29 +0300

    Some personal amir stuff

---
+++ personal/personal.el
@@ -0,0 +1,7 @@
+(key-chord-define-global "`j" 'avy-goto-char)
+(key-chord-define-global "`k" 'avy-goto-word-0-above)
+(key-chord-define-global "`j" 'avy-goto-word-0-below)
+(key-chord-define-global "jk" nil)
+(key-chord-define-global "jl" nil)
+
+;; (disable-theme 'zenburn)
