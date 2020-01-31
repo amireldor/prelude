bddf72fdeb077eb71a44c9f4fa46d4e18b486ecc
Amir Eldor <amir@eize.ninja>
2020-01-31 09:58:36 +0200

    Add things neotree rainbox things

--- personal/personal.el
+++ personal/personal.el
@@ -18,12 +18,14 @@
 (evil-leader/set-key
   "5" 'find-prelude-personal)
 
-(evil-leader/set-key-for-mode 'clojure-mode
-  "d" 'cider-find-var
-  "D" 'cider-doc
-  "fn" 'cider-find-ns
-  "z" 'cider-switch-to-repl-buffer
-  "ji" 'cider-jack-in)
+(dolist (mode '(clojure-mode clojurescript-mode clojurec-mode))
+  (evil-leader/set-key-for-mode mode
+    "d" 'cider-find-var
+    "D" 'cider-doc
+    "bn" 'cider-browse-ns
+    "z" 'cider-switch-to-repl-buffer
+    "ji" 'cider-jack-in))
+
 
 (use-package parinfer
   :ensure t
@@ -33,11 +35,11 @@
   (progn
     (setq parinfer-extensions
           '(defaults       ; should be included.
-            pretty-parens  ; different paren styles for different modes.
-            evil           ; If you use Evil.
-            paredit        ; Introduce some paredit commands.
-            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-            smart-yank))   ; Yank behavior depend on mode.
+             pretty-parens  ; different paren styles for different modes.
+             evil           ; If you use Evil.
+             paredit        ; Introduce some paredit commands.
+             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
@@ -51,10 +53,10 @@
     (global-evil-leader-mode)
     (evil-leader/set-leader "<SPC>")
     (evil-leader/set-key
-    "b" 'switch-to-buffer
-    "w" 'save-buffer
-    "q" 'kill-buffer-and-window
-    "f" 'projectile--find-file)))
+      "b" 'switch-to-buffer
+      "w" 'save-buffer
+      "q" 'kill-buffer-and-window
+      "f" 'projectile--find-file)))
 
 (use-package evil-escape
   :ensure t
@@ -68,3 +70,38 @@
 
 (use-package afternoon-theme
   :ensure t)
+
+(defun my-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(use-package clj-refactor
+  :ensure t
+  :init
+  (progn
+    (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)))
+
+(use-package neotree
+  :ensure t
+  :init
+  (progn
+    (evil-leader/set-key "3" 'neotree-toggle)
+    (add-hook 'neotree-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))))
+
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)))
