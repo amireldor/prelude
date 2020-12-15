23e9f5d4d5f1a32f9b42e84e9d91536f8f90d122
Amir Eldor <amir@eldor.dev>
2020-12-15 19:53:08 +0200

    Add red modeline when in god-mode

--- personal/amir.el
+++ personal/amir.el
@@ -7,3 +7,15 @@
 (global-set-key (kbd "C-x C-2") #'split-window-below)
 (global-set-key (kbd "C-x C-3") #'split-window-right)
 (global-set-key (kbd "C-x C-0") #'delete-window)
+
+(defun my-god-mode-update-modeline ()
+  (let ((limited-colors-p (> 257 (length (defined-colors)))))
+    (cond (god-local-mode (progn
+                            (set-face-background 'mode-line (if limited-colors-p "white" "red"))
+                            (set-face-background 'mode-line-inactive (if limited-colors-p "white" "#e9e2cb"))))
+          (t (progn
+               (set-face-background 'mode-line (if limited-colors-p "black" "#0a2832"))
+               (set-face-background 'mode-line-inactive (if limited-colors-p "black" "#0a2832")))))))
+
+(add-hook 'god-mode-enabled-hook #'my-god-mode-update-modeline)
+(add-hook 'god-mode-disabled-hook #'my-god-mode-update-modeline)
